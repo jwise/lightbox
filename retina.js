825637095ff4fb25aa2a80fb1c594c02d6d34ed8
Joshua Wise <joshua@joshuawise.com>
2012-10-31 22:50:27 -0700

    Retina version from Gareth Emery show

---
+++ retina.js
@@ -0,0 +1,98 @@
+// From https://github.com/imulus/retinajs, though hacked not to bother
+// checking -- we know that 2x scaled images will be around.
+
+(function() {
+
+  var root = (typeof exports == 'undefined' ? window : exports);
+
+  var config = {
+    // Ensure Content-Type is an image before trying to load @2x image
+    // https://github.com/imulus/retinajs/pull/45)
+    check_mime_type: true
+  };
+
+
+
+  root.Retina = Retina;
+
+  function Retina() {}
+
+  Retina.configure = function(options) {
+    if (options == null) options = {};
+    for (var prop in options) config[prop] = options[prop];
+  };
+
+  Retina.init = function(context) {
+    if (context == null) context = root;
+
+    var existing_onload = context.onload || new Function;
+
+    context.onload = function() {
+      var images = document.getElementsByTagName("img"), retinaImages = [], i, image;
+      for (i = 0; i < images.length; i++) {
+        image = images[i];
+        retinaImages.push(new RetinaImage(image));
+      }
+      existing_onload();
+    }
+  };
+
+  Retina.isRetina = function(){
+    var mediaQuery = "(-webkit-min-device-pixel-ratio: 1.5),\
+                      (min--moz-device-pixel-ratio: 1.5),\
+                      (-o-min-device-pixel-ratio: 3/2),\
+                      (min-resolution: 1.5dppx)";
+
+    if (root.devicePixelRatio > 1)
+      return true;
+
+    if (root.matchMedia && root.matchMedia(mediaQuery).matches)
+      return true;
+
+    return false;
+  };
+
+
+  root.RetinaImagePath = RetinaImagePath;
+
+  function RetinaImagePath(path) {
+    this.path = path;
+    this.at_2x_path = path.replace(/\.\w+$/, function(match) { return "@2x" + match; });
+  }
+
+  RetinaImagePath.confirmed_paths = [];
+
+  RetinaImagePath.prototype.is_external = function() {
+    return !!(this.path.match(/^https?\:/i) && !this.path.match('//' + document.domain) )
+  }
+
+  function RetinaImage(el) {
+    this.el = el;
+    this.path = new RetinaImagePath(this.el.getAttribute('src'));
+    this.swap();
+  }
+
+  root.RetinaImage = RetinaImage;
+
+  RetinaImage.prototype.swap = function(path) {
+    if (typeof path == 'undefined') path = this.path.at_2x_path;
+
+    var that = this;
+    function load() {
+      if (! that.el.complete) {
+        setTimeout(load, 5);
+      } else {
+        that.el.setAttribute('width', that.el.width);
+        that.el.setAttribute('height', that.el.height);
+        that.el.setAttribute('src', path);
+      }
+    }
+    load();
+  }
+
+  if (Retina.isRetina()) {
+    Retina.init(root);
+  }
+
+})();
+
